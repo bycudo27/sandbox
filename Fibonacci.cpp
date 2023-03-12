95677b41c0ba783260ee14bd0cff0d1b3035c6c2
bycudo27 <81246681+bycudo27@users.noreply.github.com>
2023-03-12 21:54:37 +0300

    Add files via upload

---
+++ Fibonacci.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <math.h>
+#include <conio.h>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+	long a, b, f1, f2 = 0, f3 = 1;
+	cout << "Enter the left border" << endl;
+	cin >> a;
+	cout << "Enter the right border" << endl;
+	cin >> b;
+	while (f2 <= b)
+	{
+		if (f2 >= a)
+		{
+			cout << f2 << " " << endl;
+			f1 = f2;
+			f2 = f3;
+			f3 = f1 + f2;
+		}
+	}
+	return 0;
+}
---
+++ object.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+	if (SetConsoleCP(1251) == 0)
+	{
+		cerr << "Failed to set codepage!" << endl;
+	}
+	if (SetConsoleOutputCP(1251) == 0)
+	{
+		cerr << "Failedto set OUTPUT codepage!" << endl;
+	}
+	class Infa
+	{
+	public:
+		string name, surname, age, height;
+
+
+		void input_infa(string imya, string family, string vozr, string rost)
+		{
+			name = imya;
+			surname = family;
+			age = vozr;
+			height = rost;
+		}
+		void output_infa()
+		{
+			cout << "Введите имя: " << name << endl;
+			cout << "Введите фамилию" << surname << endl;
+			cout << "Введите возраст: " << age << endl;
+			cout << "Введите вес: " << height << endl;
+		}
+	};
+
+	Infa one;
+	Infa two;
+
+	string imya, family, vozr, rost;
+	imya = "Кадум";
+	family = "Вокболок";
+	vozr = "47 лет";
+	rost = "1.86 м";
+	one.input_infa(imya, family, vozr, rost);
+	cout << "Объект 1" << endl;
+	one.output_infa();
+	cout << endl;
+
+	imya = "Николай";
+	family = "Митягин";
+	vozr = "28 лет";
+	rost = "2.00 м";
+	two.input_infa(imya, family, vozr, rost);
+	cout << "\n\nОбъект 2" << endl;
+	two.output_infa();
+}
